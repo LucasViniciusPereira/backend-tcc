8c0cd6499a40c3a9bcdb09469922d96bc8c43213
Wilson Donizetti de Noronha Junior <wilsondonizetti@hotmail.com>
2017-08-20 19:53:15 -0300

    alterando para apenas um projeto de api... problema de disponibiliadade de subdominios

---
+++ backend-tcc/Controllers/Estoques/ProdutoController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace backend_tcc.ms.administrador.Controllers
+{
+    public class ProdutoController : ApiController
+    {
+    }
+}
